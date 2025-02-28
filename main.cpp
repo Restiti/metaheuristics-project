4320794fed670c0baa0d272410df167ac0e45d41
remy <rem59570@gmail.com>
2025-02-28 10:33:16 +0100

    feat: read data from txt

--- main.cpp
+++ main.cpp
@@ -1,25 +1,84 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+// Structure pour stocker les données du problème
+struct SetCoverProblem {
+    int m; // Nombre de lignes (éléments de l'univers U)
+    int n; // Nombre de colonnes (sous-ensembles)
+    vector<int> costs; // Coût de chaque sous-ensemble
+    vector<vector<int> > A; // Matrice de couverture binaire
+};
+
+// Fonction pour lire un fichier SCP et charger les données dans une structure
+SetCoverProblem read_scp_file(const string& filename) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Erreur: Impossible d'ouvrir le fichier " << filename << endl;
+        exit(1);
     }
+
+    SetCoverProblem problem;
+
+    // Lire m et n (nombre de lignes et colonnes)
+    file >> problem.m >> problem.n;
+
+    // Lire les coûts de chaque colonne
+    problem.costs.resize(problem.n);
+    for (int j = 0; j < problem.n; j++) {
+        file >> problem.costs[j];
+    }
+
+    // Initialiser la matrice de couverture avec des 0
+    problem.A.assign(problem.m, vector<int>(problem.n, 0));
+
+    // Lire les lignes et leurs colonnes couvertes
+    for (int i = 0; i < problem.m; i++) {
+        int num_covering_columns;
+        file >> num_covering_columns; // Nombre de colonnes couvrant la ligne i
+
+        for (int k = 0; k < num_covering_columns; k++) {
+            int col_index;
+            file >> col_index;
+            problem.A[i][col_index - 1] = 1; // Les indices dans le fichier commencent à 1
+        }
+    }
+
+    file.close();
+    return problem;
+}
+
+// Fonction pour afficher les données lues (debug)
+void display_problem(const SetCoverProblem& problem) {
+    cout << "Nombre de lignes (m) : " << problem.m << endl;
+    cout << "Nombre de colonnes (n) : " << problem.n << endl;
+
+    cout << "Coûts des sous-ensembles : ";
+    for (int cost : problem.costs) {
+        cout << cost << " ";
+    }
+    cout << endl;
+
+    cout << "Matrice de couverture : " << endl;
+    for (const auto& row : problem.A) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    string filename = "resources/scp41.txt"; // Remplacez par le fichier à lire
+
+    // Lire les données
+    SetCoverProblem problem = read_scp_file(filename);
+
+    // Afficher les données lues
+    display_problem(problem);
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
